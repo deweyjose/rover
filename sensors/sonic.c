85eceec2a55d28af2314decbbe8814572897703f
deweyjose <richard.e.jose@gmail.com>
2016-02-04 23:28:22 -0500

    simplified the code, run each range finder in its own process

--- sensors/sonic.c
+++ sensors/sonic.c
@@ -28,35 +28,19 @@
 #define BILLION         1E9
 #define TRIGGER_DELAY   10
 
-#define TRIGGER_FRONT   4
-#define TRIGGER_REAR    7
-
-#define ECHO_FRONT      5
-#define ECHO_REAR       6
-
-#define STOP_FORWARD    "stop_forward";
-#define STOP_REVERSE    "stop_reverse";
-
 // =================================================
 
-struct monitorspec {
-    const char *    command;
-    int             trigger;
-    int             echo;
-    struct timespec start;
-    struct timespec stop;
-    int             violations;
-    pthread_mutex_t lock;
-    void *          func;
-};
-
-// =================================================
-
-struct monitorspec  g_front;
-struct monitorspec  g_rear;
-
-double              g_min_distance = 0.0;
+int                 g_echo         = 0;
+int                 g_trigger      = 0;
+int                 g_violations   = 0;
 int                 g_filter_count = 0;
+double              g_min_distance = 0.0;
+bool                g_up           = false;
+char                g_direction[30];
+char                g_command[30];
+struct timespec     g_start;
+struct timespec     g_stop;
+pthread_mutex_t     g_lock;
 redisContext *      g_context      = NULL;
 
 // =================================================
@@ -65,7 +49,7 @@ void read_clock(struct timespec * ts) {
     if (clock_gettime(CLOCK_REALTIME, ts) == -1) {
         fprintf(stderr, "Unable to get realtime clock: %s\n", strerror (errno)) ;
         exit(EXIT_FAILURE);
-     }
+    }
 }
 
 double calculate_distance(struct timespec * start, struct timespec * stop) {
@@ -74,52 +58,47 @@ double calculate_distance(struct timespec * start, struct timespec * stop) {
 }
 
 bool valid_distance(double distance) {
-    return distance > 2 && distance < 400;
+    return distance > 0 && distance < 1000;
 }
 
 // =================================================
 
-void trigger(struct monitorspec * spec) {
-    digitalWrite(spec->trigger, 1); 
+void trigger() {
+    digitalWrite(g_trigger, 1); 
     delayMicroseconds(TRIGGER_DELAY);
-    digitalWrite(spec->trigger, 0);
+    digitalWrite(g_trigger, 0);
 }
 
 // =================================================
 
-void monitor(struct monitorspec * spec) {    
-    if (digitalRead(spec->echo) == 1) {
-        read_clock(&spec->start);                       
-    } else {        
-        read_clock(&spec->stop);       
-        double distance = calculate_distance(&spec->start, &spec->stop);
-        if (valid_distance(distance)) {
-            pthread_mutex_lock(&spec->lock);
-            if (distance < g_min_distance) {
-                spec->violations++;
-                if (spec->violations > g_filter_count) {                    
-                    if (rand() % 100 < 10) {
-                        fprintf(stderr, "%d violations. distance %lf, sending %s\n", spec->violations, distance, spec->command);
+void monitor() {  
+    if (digitalRead(g_echo) == 1) {
+        read_clock(&g_start);
+        g_up = true;
+    } else {
+        if (g_up) {
+            read_clock(&g_stop);
+            double distance = calculate_distance(&g_start, &g_stop);
+            if (valid_distance(distance)) {
+                if (distance < g_min_distance) {
+                    g_violations++;
+                    if (g_violations > g_filter_count) {
+                        if (rand() % 100 < 10) {
+                            fprintf(stderr, "%d violations. distance %lf, sending %s\n", g_violations, distance, g_command);
+                        }
+                        char buffer[30];
+                        sprintf(buffer, "PUBLISH rover %s", g_command);
+                        redisCommand(g_context, buffer);
+                        g_violations = 0;
                     }
-                    char buffer[30];
-                    sprintf(buffer, "PUBLISH rover %s", spec->command);
-                    redisCommand(g_context, buffer);
-                    spec->violations = 0;
+                } else {
+                    g_violations = 0;
                 }
-            } else {
-                spec->violations = 0;
             }
-            pthread_mutex_unlock(&spec->lock);
         }
-    }
-}
-
-void monitor_front(void) {    
-    monitor(&g_front);
-}
-
-void monitor_rear(void) {    
-    monitor(&g_rear);    
+        
+        g_up = false;     
+    }    
 }
 
 // =================================================
@@ -141,32 +120,21 @@ bool setup_wiring_pi() {
         return false;
     }
     
-    if (piHiPri(50) < 0) {
-        fprintf(stderr, "Unable to set priority to 50: %s\n", strerror(errno));
-        return false;
-    }    
-    
-    return true;
-}
-
-// =================================================
-
-bool initialize_monitorspec(struct monitorspec * spec, int trigger, int echo) {
-    if (pthread_mutex_init(&spec->lock, NULL) != 0){
-        printf("mutex init failed\n");
-        return false;
-    }
-    spec->trigger = trigger;
-    spec->echo    = echo;
-    
-    pinMode(spec->trigger, OUTPUT);
-    
-    if (wiringPiISR (spec->echo, INT_EDGE_BOTH, spec->func) < 0) {
+    if (wiringPiISR (g_echo, INT_EDGE_BOTH, &monitor) < 0) {
         fprintf(stderr, "Unable to setup ISR: %s\n", strerror(errno));
         return false;
     }
     
-    digitalWrite(spec->trigger, 0); delay(1000);
+    pinMode(g_trigger, OUTPUT);
+    
+    digitalWrite(g_trigger, 0); 
+    
+    delay(1000);   
+    
+    if (piHiPri(50) < 0) {
+        fprintf(stderr, "Unable to set priority to 50: %s\n", strerror(errno));
+        return false;
+    }    
     
     return true;
 }
@@ -181,7 +149,7 @@ void usage() {
 // =================================================
 
 int main(int argc, char** argv) {        
-    if (argc != 6) {
+    if (argc != 9) {
         usage();
         return (EXIT_FAILURE);
     }
@@ -190,9 +158,16 @@ int main(int argc, char** argv) {
     int    redis_port     = atoi(argv[2]);
            g_min_distance = atof(argv[3]);
            g_filter_count = atoi(argv[4]);
-    int    reading_delay  = atoi(argv[5])/2;
-      
-    printf("%s:%d, min %lf, filter %d, delay %d\n", redis_server, redis_port, g_min_distance, g_filter_count, reading_delay);
+    int    reading_delay  = atoi(argv[5]);
+           g_trigger      = atoi(argv[6]);
+           g_echo         = atoi(argv[7]); 
+           g_up           = false;
+           
+           sprintf(g_direction, "%s", argv[8]);
+           sprintf(g_command, "stop_%s", g_direction);
+           
+    printf("%s:%d, min %lf, filter %d, delay %d, direction %s, trigger %d, echo %d\n", 
+            redis_server, redis_port, g_min_distance, g_filter_count, reading_delay, g_direction, g_trigger, g_echo);
     
     g_context = rconnect(redis_server, redis_port);
     
@@ -200,18 +175,14 @@ int main(int argc, char** argv) {
         return (EXIT_FAILURE);
     }        
     
-    g_front.func = monitor_front;
-    g_rear .func = monitor_rear;
-    
-    g_front.command = STOP_FORWARD;
-    g_rear .command = STOP_REVERSE;
-    
-    if (!initialize_monitorspec(&g_front, TRIGGER_FRONT, ECHO_FRONT)) { return (EXIT_FAILURE); }
-    if (!initialize_monitorspec(&g_rear , TRIGGER_REAR , ECHO_REAR )) { return (EXIT_FAILURE); }
+    if (pthread_mutex_init(&g_lock, NULL) != 0){
+        printf("mutex init failed\n");
+        return false;
+    }     
     
     while(true) { 
-        trigger(&g_front); delay(reading_delay);
-        trigger(&g_rear ); delay(reading_delay);
+        trigger(); 
+        delay(reading_delay);       
     }
     
     return (EXIT_SUCCESS);
